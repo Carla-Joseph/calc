bed8a94ab82398fba6708be80828935e389ecea8
Carla Joseph <carlajseph@gmail.com>
2020-07-01 18:28:31 -0400

    Created Calculator function

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import HelloWorld from '../components/HelloWorld'
+import Calculator from '../components/Calculator'
 
 function HomePage() {
-  return <HelloWorld />
+  return <Calculator />
 }
 
 export default HomePage
